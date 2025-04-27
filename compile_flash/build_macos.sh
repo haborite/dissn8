7d40a2d6743eb2ce372aac2f6bd6d76025e6456a
haborite <korintje@korintjenoMac-mini.local>
2025-04-27 19:00:37 +0900

    Change macOS setting

--- compile_flash/build_macos.sh
+++ compile_flash/build_macos.sh
@@ -4,6 +4,6 @@ SCRIPT_NAME="../sn8/flashsn8.py"
 OUTPUT_NAME="flashsn8"
 
 # Run Nuitka to build the executable
-nuitka --standalone --remove-output --output-dir=linux_x86-64 --output-filename=$OUTPUT_NAME --assume-yes-for-downloads --user-package-configuration-file=config.yaml $SCRIPT_NAME
+nuitka --standalone --remove-output --output-dir=macos_x86-64 --output-filename=$OUTPUT_NAME --assume-yes-for-downloads --user-package-configuration-file=config.yaml $SCRIPT_NAME
 
 echo "Build complete: $OUTPUT_NAME"
