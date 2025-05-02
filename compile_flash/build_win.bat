6408af8ac593cabd69629bec5facc0dd4895b5d2
haborite <korintje@ymail.ne.jp>
2025-05-03 00:56:19 +0900

    Update bild_win.bat

--- compile_flash/build_win.bat
+++ compile_flash/build_win.bat
@@ -1,11 +1,17 @@
 @echo off
 :: Set the script filename
-set SCRIPT_NAME=flashsn8.py
+set SCRIPT_NAME=..\sn8\flashsn8.py
 set OUTPUT_NAME=flashsn8.exe
 set OUTDIR_NAME=win_x86-64
+set ZIP_NAME=windows_x86-64.zip
 
 :: Run Nuitka to build the executable
 nuitka --standalone --remove-output --output-dir=%OUTDIR_NAME% --output-filename=%OUTPUT_NAME% --assume-yes-for-downloads --user-package-configuration-file=config.yaml %SCRIPT_NAME%
 
 echo Build complete: %OUTPUT_NAME%
-pause
+
+:: Create ZIP file (overwrite if exists)
+powershell -Command "Compress-Archive -Path '%OUTDIR_NAME%\*' -DestinationPath '%ZIP_NAME%' -Force"
+
+echo ZIP archive created: %ZIP_NAME%
+
